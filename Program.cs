9952d0f770f28350d3ee44bad6831982b8ce0b09
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2021-09-30 18:45:05 +0300

    git

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
